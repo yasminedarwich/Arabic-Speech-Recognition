43372320a6ce2f9a350f3878f82cc72e93bf2382
Yasmine D <95974713+yasminedarwich@users.noreply.github.com>
2023-12-09 19:26:37 +0200

    Update NewStreamlit.py

--- NewStreamlit.py
+++ NewStreamlit.py
@@ -77,17 +77,6 @@ st.markdown(
 
 # Create a Streamlit app
 
-# Create columns
-col1, col2 = st.columns(2)  # Two columns with equal width
-
-# Add image to first column
-with col1:
-    st.image("podeo-logo-english-white.png", width=300)
-
-# Add title to second column
-with col2:
-    st.title("Audio-Transcription App")
-
 # Create a sidebar for navigation
 st.sidebar.title("Hey there! 👋🏻")
 with st.sidebar:
@@ -101,6 +90,20 @@ with st.sidebar:
 page = st.sidebar.radio("Select a Page", ["Home💫", "Transcription🎤", "Analytics📊"])
 
 if page == "Home💫":
+
+    
+
+# Create columns
+col1, col2 = st.columns(2)  # Two columns with equal width
+
+# Add image to first column
+with col1:
+    st.image("podeo-logo-english-white.png", width=300)
+
+# Add title to second column
+with col2:
+    st.title("Audio-Transcription App")
+    
     # This is the introduction page
     st.markdown(
         """
