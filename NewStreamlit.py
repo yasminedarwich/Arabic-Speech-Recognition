868beef4169fa606a439899afa3738a9b186f1f2
Yasmine D <95974713+yasminedarwich@users.noreply.github.com>
2023-12-09 18:23:05 +0200

    Update NewStreamlit.py

--- NewStreamlit.py
+++ NewStreamlit.py
@@ -102,8 +102,8 @@ if page == "Home💫":
 
     # Add images, GIFs, or any content you want to display on the introduction page
     col1, col2, col3 = st.columns([0.2, 0.6, 0.2])
-with col2:
-    st.image("Podcastcaptions.png")
+    with col2:
+        st.image("Podcastcaptions.png")
 
    
 
