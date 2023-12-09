4af5a810f32220ccb1cb823a56fe48dfb1034bff
Yasmine D <95974713+yasminedarwich@users.noreply.github.com>
2023-12-09 19:22:25 +0200

    Update NewStreamlit.py

--- NewStreamlit.py
+++ NewStreamlit.py
@@ -90,16 +90,14 @@ with col2:
 
 # Create a sidebar for navigation
 st.sidebar.title("Hey there! 👋🏻")
-st.sidebar.write('''
-    I am Yasmine Darwich, an MSBA graduate at AUB 👱‍♀️🏫! 
+st.sidebar.write(' I am Yasmine Darwich, an MSBA graduate at AUB 👱‍♀️🏫! 
 
-    I enjoy working with datasets and analyzing information 🎯. 
-    
-    Let's connect:
-
-    [LinkedIn](https://www.linkedin.com/in/yasmine-darwich/)
-
-    ''')
+                 I enjoy working with datasets and analyzing information 🎯. 
+                 
+                 Let's connect:
+                 
+                 [LinkedIn](https://www.linkedin.com/in/yasmine-darwich/)
+                 ')
     
 page = st.sidebar.radio("Select a Page", ["Home💫", "Transcription🎤", "Analytics📊"])
 
