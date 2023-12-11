25bd64edb37cedc249cec47e48cfc7c7289e0a06
Yasmine D <95974713+yasminedarwich@users.noreply.github.com>
2023-12-12 01:38:45 +0200

    Update 02_Analytics📊.py

--- pages/02_Analytics📊.py
+++ pages/02_Analytics📊.py
@@ -1,10 +1,16 @@
 import streamlit as st
 
-def calculate_transcription_cost(duration_minutes, cost_per_minute):
+def calculate_transcription_cost(duration_minutes, cost_per_minute, audio_quality, num_speakers, urgency):
     try:
         duration_minutes = float(duration_minutes)
         cost_per_minute = float(cost_per_minute)
-        transcription_cost = duration_minutes * cost_per_minute
+
+        # Adjust transcription cost based on factors
+        audio_quality_factor = 1 + (audio_quality - 5) / 10  # Adjust based on a scale of 1-10
+        num_speakers_factor = 1 + (num_speakers - 1) * 0.2  # Adjust based on the number of speakers
+        urgency_factor = 1 + (urgency - 1) * 0.1  # Adjust based on urgency/turnaround time
+
+        transcription_cost = duration_minutes * cost_per_minute * audio_quality_factor * num_speakers_factor * urgency_factor
         return transcription_cost
     except ValueError:
         return None
@@ -15,13 +21,18 @@ def main():
     # Input
     duration_minutes = st.text_input("Enter Duration (minutes):", "0")
 
+    # Sliders for additional factors
+    audio_quality = st.slider("Audio Quality (1-10)", min_value=1, max_value=10, value=5)
+    num_speakers = st.slider("Number of Speakers", min_value=1, max_value=5, value=1)
+    urgency = st.slider("Urgency/Turnaround Time (1-5)", min_value=1, max_value=5, value=3)
+
     # Calculate
     cost_per_minute = 2.5  # Cost of Arabic transcription per minute
-    transcription_cost = calculate_transcription_cost(duration_minutes, cost_per_minute)
+    transcription_cost = calculate_transcription_cost(duration_minutes, cost_per_minute, audio_quality, num_speakers, urgency)
 
     # Display Result
     if transcription_cost is not None:
-        st.subheader("The estimated transcription cost is:")
+        st.subheader("Estimated Transcription Cost:")
         st.write(f"${transcription_cost:.2f}")
     else:
         st.warning("Please enter a valid numerical value for duration.")
