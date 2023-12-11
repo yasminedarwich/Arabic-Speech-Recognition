5837d9a2813885be272efc1079f4bddaffb1f179
Yasmine D <95974713+yasminedarwich@users.noreply.github.com>
2023-12-11 02:27:05 +0200

    Update 01_Transcribe🎤.py

--- pages/01_Transcribe🎤.py
+++ pages/01_Transcribe🎤.py
@@ -86,16 +86,7 @@ with col2:
 
 # Create a sidebar for navigation
 st.sidebar.title("Hey there! 👋🏻")
-st.sidebar.write('''
-    I am Yasmine Darwich, an MSBA student at AUB 👱‍♀️🏫! 
 
-    I enjoy working with datasets and analyzing information 🎯. 
-    
-    Let's connect:
-
-    [LinkedIn](https://www.linkedin.com/in/yasmine-darwich/)
-
-    ''')
 
 # This is the transcription page
 
