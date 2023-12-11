bac04a256bf1025ecee53c159bf0118b28abbe31
Yasmine D <95974713+yasminedarwich@users.noreply.github.com>
2023-12-11 08:57:31 +0200

    Update 04_Sentiment.py

--- pages/04_Sentiment.py
+++ pages/04_Sentiment.py
@@ -1,42 +1,22 @@
 import streamlit as st
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from arabert import ArabertPreprocessor
-from arabert.aragpt2.grover.modeling_gpt2 import GPT2LMHeadModel
+import ktrain
+from ktrain import text
 
-# Load AraBERT model and tokenizer
-tokenizer = AutoTokenizer.from_pretrained("aubmindlab/bert-base-arabertv2")
-model = AutoModelForSequenceClassification.from_pretrained("aubmindlab/bert-base-arabertv2-sentiment")
+# Load the saved predictor
+MODEL_NAME = 'aubmindlab/bert-base-arabertv01'
+p = ktrain.load_predictor('/tmp/arabic_predictor')
 
-# Define sentiment labels
-labels = ["NEGATIVE", "NEUTRAL", "POSITIVE"]
+# Streamlit app
+def main():
+    st.title("Arabic Sentiment Analysis with BERT")
 
-def predict_sentiment(text):
-    """
-    Predicts the sentiment of a given text using AraBERT.
-    """
-    encoded_text = tokenizer(text, return_tensors="pt")
-    output = model(**encoded_text)
-    logits = output.logits.squeeze(0)
-    predicted_label_id = logits.argmax().item()
-    predicted_label = labels[predicted_label_id]
-    return predicted_label
+    # Input text
+    input_text = st.text_area("Enter Arabic text:", "الغرفة كانت نظيفة ، الطعام ممتاز ، وأنا أحب المنظر من غرفتي.")
 
-# Streamlit app layout
-st.title("AraBERT Sentiment Analysis")
-st.write("Enter your text below to get sentiment analysis.")
+    # Analyze sentiment on button click
+    if st.button("Analyze Sentiment"):
+        result = p.predict(input_text)
+        st.write(f"Sentiment: {result}")
 
-text = st.text_area("Input Text")
-
-if st.button("Analyze Sentiment"):
-    if text:
-        predicted_sentiment = predict_sentiment(text)
-        st.write(f"Sentiment: **{predicted_sentiment}**")
-    else:
-        st.write("Please enter some text to analyze.")
-
-st.markdown(
-    """
-    This app uses AraBERT, a pre-trained Transformer model for Arabic language understanding,
-    to perform sentiment analysis on the input text.
-    """
-)
+if __name__ == "__main__":
+    main()
