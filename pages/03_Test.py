6b153b210e85dcad65c08048d793a7d2e501164a
Yasmine D <95974713+yasminedarwich@users.noreply.github.com>
2023-12-11 06:40:10 +0200

    Update 03_Test.py

--- pages/03_Test.py
+++ pages/03_Test.py
@@ -199,7 +199,7 @@ df8 = pd.read_csv("https://raw.githubusercontent.com/yasminedarwich/Arabic-Speec
 st.title('Visualizing Listens Count by Country')
 
 # Visualization 1: Bar chart
-bar_chart = px.bar(df8, x='country', y='listens_count', title='Listens Count by Country',
+bar_chart = px.bar(df8, x='Country', y='listens_count', title='Listens Count by Country',
                    labels={'listens_count': 'Listens Count'},
                    color='listens_count', color_continuous_scale='Viridis')
 st.plotly_chart(bar_chart)
